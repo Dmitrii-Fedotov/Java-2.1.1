ee26c9c61877856925319796aa855ced03e2fb92
Dmitrii Fedotov <fedotovdmz@gmail.com>
2021-08-19 14:05:24 +0300

    initial comit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 2543;
+        int wasted = 20;
+        int totalMile = price / wasted;
+        System.out.println("За вашу покупку вы получите:" + " " + totalMile + " " + "миль");
+    }
+}
